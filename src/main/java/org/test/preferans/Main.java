646636b7bd9e880769dcf4c9eefec74e29dd072d
aurelgava <gava1977@gmail.com>
2023-11-09 10:45:25 +0100

    ispis karata

--- src/main/java/org/test/preferans/Main.java
+++ src/main/java/org/test/preferans/Main.java
@@ -7,18 +7,16 @@ import org.preferans.igraci.IgracAI;
 public class Main {
     public static final byte BROJ_IGRACA = 3;
     public static void main(String[] args) {
-        zapocniNovuSesiju((short)100);
-    }
-
-    private static void zapocniNovuSesiju(short bule) {
-
-        Partija s = Partija.getNewInstance();
-
+        short bule = 100;
         Igrac[] igraci = new Igrac[BROJ_IGRACA];
         igraci[0] = new IgracAI("Predrag",bule);
         igraci[1] = new IgracAI("Frantz",bule);
         igraci[2] = new IgracAI("Eintz",bule);
+        zapocniNovuSesiju(igraci);
+    }
 
+    private static void zapocniNovuSesiju(Igrac[] igraci) {
+        Partija s = Partija.getNewInstance();
         s.getAktivnoDeljenje().podeliKarte(igraci);
 
     }
